0f756d9808cf90047aa4e03b32e3a5eb9f346fed
cyli8 <cyli8@iflytek.com>
2018-08-29 20:43:06 +0800

    Constraint布局初探

---
+++ app/src/main/java/com/lzy/studysource/constraint/ConstraintActivity.java
@@ -0,0 +1,15 @@
+package com.lzy.studysource.constraint;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lzy.studysource.R;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint);
+    }
+}
