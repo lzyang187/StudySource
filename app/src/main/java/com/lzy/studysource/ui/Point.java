1cec98703b10c9346ab4fe507db1ec89a9dcbd86
cyli8 <cyli8@iflytek.com>
2018-10-12 16:37:30 +0800

    仿蚂蚁森林View

---
+++ app/src/main/java/com/lzy/studysource/ui/Point.java
@@ -0,0 +1,16 @@
+package com.lzy.studysource.ui;
+
+/**
+ * @author: cyli8
+ * @date: 2018/10/11 19:44
+ */
+public class Point {
+    public float startX;
+    public float startY;
+
+    public Point(float startX, float startY) {
+        this.startX = startX;
+        this.startY = startY;
+
+    }
+}
