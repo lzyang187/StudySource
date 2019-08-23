e21c34c83d43bce9714bc6cf90ae8fbc42cbbd37
cyli8 <cyli8@iflytek.com>
2019-08-23 11:05:03 +0800

    CardView相关用法

---
+++ app/src/main/java/com/lzy/studysource/materialdesign/CardViewActivity.java
@@ -0,0 +1,16 @@
+package com.lzy.studysource.materialdesign;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.lzy.studysource.R;
+
+public class CardViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
