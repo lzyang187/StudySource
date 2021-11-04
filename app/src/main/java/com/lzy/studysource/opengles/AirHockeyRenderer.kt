3395a7d74cf608be3d447764ac0f961cf60ab636
cyli8 <cyli8@iflytek.com>
2021-11-04 22:45:01 +0800

    feat:三角形扇、增加颜色属性、着色（混合色）

--- app/src/main/java/com/lzy/studysource/opengles/AirHockeyRenderer.kt
+++ app/src/main/java/com/lzy/studysource/opengles/AirHockeyRenderer.kt
@@ -22,34 +22,51 @@ class AirHockeyRenderer(private val mContext: Context) : GLSurfaceView.Renderer 
     companion object {
         private const val TAG = "AirHockeyRenderer"
 
-        // 每个顶点的浮点数
-        const val POSITION_COMPONENT_COUNT = 2
+        // 每个顶点中位置的浮点数
+        private const val POSITION_COMPONENT_COUNT = 2
+
+        // 每个顶点中颜色的浮点数
+        private const val COLOR_COMPONENT_COUNT = 3
 
         // 每个float占用的字节
-        const val BYTES_PER_FLOAT = 4
+        private const val BYTES_PER_FLOAT = 4
+
         private const val A_POSITION = "a_Position"
-        private const val U_COLOR = "u_Color"
+        private const val A_COLOR = "a_Color"
+
+        // 跨距，告诉OpenGL每个位置之间有多少个字节
+        private const val STRIDE =
+            (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT
     }
 
     // 两个三角形组成的长方形桌子的顶点
     private val mTableVerticesWithTriangles = floatArrayOf(
         // 第一个三角形，逆时针排列
-        -0.5f, -0.5f,
-        0.5f, 0.5f,
-        -0.5f, 0.5f,
+//        -0.5f, -0.5f,
+//        0.5f, 0.5f,
+//        -0.5f, 0.5f,
 
         // 第二个三角形
-        -0.5f, -0.5f,
-        0.5f, -0.5f,
-        0.5f, 0.5f,
+//        -0.5f, -0.5f,
+//        0.5f, -0.5f,
+//        0.5f, 0.5f,
+
+        // 三角形扇的方式
+        // x, y, r, g, b
+        0f, 0f, 1f, 1f, 1f,
+        -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+        0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+        0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+        -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+        -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
 
         // 中间线
-        -0.5f, 0f,
-        0.5f, 0f,
+        -0.5f, 0f, 1f, 0f, 0f,
+        0.5f, 0f, 0f, 0f, 1f,
 
         // 两个木槌，用两个点代替
-        0f, -0.25f,
-        0f, 0.25f,
+        0f, -0.25f, 0f, 0f, 1f,
+        0f, 0.25f, 1f, 0f, 0f
     )
 
     // OpenGL作为本地系统库直接运行在硬件上，没有虚拟机、垃圾回收器，所以需要把内存从Java堆复制到本地堆
@@ -65,7 +82,7 @@ class AirHockeyRenderer(private val mContext: Context) : GLSurfaceView.Renderer 
 
     private var mProgramId = 0
     private var mAPositionLocation = 0
-    private var mUColorLocation = 0
+    private var mAColorLocation = 0
 
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
@@ -85,17 +102,28 @@ class AirHockeyRenderer(private val mContext: Context) : GLSurfaceView.Renderer 
         GLES20.glUseProgram(mProgramId)
         // 获取属性位置，就能告诉OpenGL到哪里去找到这个属性对应的数据了
         mAPositionLocation = GLES20.glGetAttribLocation(mProgramId, A_POSITION)
-        // 获取uniform的位置，稍后要更新这个uniform的时候会到mAPositionLocation
-        mUColorLocation = GLES20.glGetUniformLocation(mProgramId, U_COLOR)
         // 把位置设置在数据的开头，确保OpenGL从头开始读取缓冲区的数据
         mVertexData.position(0)
         // 告诉OpenGL可以在缓冲区中找到a_Position对应的数据
         GLES20.glVertexAttribPointer(
             mAPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
-            false, 0, mVertexData
+            false, STRIDE, mVertexData
         )
         // 使能顶点数组
         GLES20.glEnableVertexAttribArray(mAPositionLocation)
+
+        // 获取uniform的位置，稍后要更新这个uniform的时候会到mAPositionLocation
+//        mUColorLocation = GLES20.glGetUniformLocation(mProgramId, U_COLOR)
+        mAColorLocation = GLES20.glGetAttribLocation(mProgramId, A_COLOR)
+        // 当OpenGL读入颜色时需要跳过位置属性
+        mVertexData.position(POSITION_COMPONENT_COUNT)
+        // 把颜色数据和片段着色器中的a_Color关联起来
+        GLES20.glVertexAttribPointer(
+            mAColorLocation, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT,
+            false, STRIDE, mVertexData
+        )
+        // 为颜色属性使能顶点属性数组
+        GLES20.glEnableVertexAttribArray(mAColorLocation)
     }
 
 
@@ -107,20 +135,21 @@ class AirHockeyRenderer(private val mContext: Context) : GLSurfaceView.Renderer 
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
         // 绘制桌子部分
         // 更新着色器代码中的u_Color值，这里是白色
-        GLES20.glUniform4f(mUColorLocation, 1.0f, 1.0f, 1.0f, 1.0f)
+//        GLES20.glUniform4f(mUColorLocation, 1.0f, 1.0f, 1.0f, 1.0f)
         // 告诉OpenGL我们要画三角形，使用顶点数组索引为0到5共六个顶点，这里会画两个三角形
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
+//        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6)
         // 绘制中间线部分，这里是红色
-        GLES20.glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
+//        GLES20.glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
         // 画线
         GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2)
         // 绘制连个木槌点
         // 绘制第一个木槌点，蓝色
-        GLES20.glUniform4f(mUColorLocation, 0.0f, 0.0f, 1.0f, 1.0f)
+//        GLES20.glUniform4f(mUColorLocation, 0.0f, 0.0f, 1.0f, 1.0f)
         // 画点
         GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1)
         // 绘制第二个木槌点，红色
-        GLES20.glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
+//        GLES20.glUniform4f(mUColorLocation, 1.0f, 0.0f, 0.0f, 1.0f)
         GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1)
 
     }
--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -1,7 +1,11 @@
 // GLSL是OpenGL的着色语言，语法结构与C语言相似
 attribute vec4 a_Position;
+attribute vec4 a_Color;
+// varying把给它的那些值进行混合，并把这些混合后的值发送给片段着色器
+varying vec4 v_Color;
 // 着色器的主要入口点
 void main() {
+    v_Color = a_Color;
     // gl_Position一定要赋值，OpenGL会把gl_Position存储的值作为当前顶点的最终位置
     gl_Position = a_Position;
     // 指定点的大小，点的矩形的边长为10
