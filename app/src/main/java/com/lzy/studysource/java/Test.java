1d6196771311a6b52fb620450054ef90a6ea31c1
cyli8 <cyli8@iflytek.com>
2022-01-15 15:33:42 +0800

    feat:自动装箱的陷阱

--- app/src/main/java/com/lzy/studysource/java/Test.java
+++ app/src/main/java/com/lzy/studysource/java/Test.java
@@ -56,5 +56,25 @@ public class Test {
             e.printStackTrace();
         }
 
+        integerFun();
+    }
+
+    /**
+     * 自动装箱的陷阱。包装类的"=="在不遇到算术运算的情况下不自动拆箱，以及equals()方法不处理数据转换的关系
+     */
+    public static void integerFun() {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
     }
 }
