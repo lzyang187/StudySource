0fd0c9c20a409f9339eb596588bed2d451da90c7
zhaoyang.li5 <zhaoyang.li5@transsion.com>
2022-03-13 17:18:02 +0800

    configChanges属性

--- app/src/main/java/com/lzy/studysource/drawable/DrawableActivity.kt
+++ app/src/main/java/com/lzy/studysource/drawable/DrawableActivity.kt
@@ -1,7 +1,9 @@
 package com.lzy.studysource.drawable
 
+import android.content.res.Configuration
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,7 @@ import com.lzy.studysource.R
 class DrawableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate: ")
         setContentView(R.layout.activity_drawable)
         val iv = findViewById<ImageView>(R.id.iv)
 //        iv.setImageLevel(1)
@@ -20,5 +23,59 @@ class DrawableActivity : AppCompatActivity() {
 
         val custom = findViewById<TextView>(R.id.custom)
         custom.background = CustomDrawable(Color.BLUE)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy: ")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState: ")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i(TAG, "onRestoreInstanceState: ")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Log.i(TAG, "onConfigurationChanged:")
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.i(TAG, "onConfigurationChanged: 横屏了")
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Log.i(TAG, "onConfigurationChanged: 竖屏了")
+        }
+    }
+
+    companion object {
+        private const val TAG = "DrawableActivity"
     }
 }
